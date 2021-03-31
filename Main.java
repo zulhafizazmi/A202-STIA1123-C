5ea130806bfb97b61cbb3a0f8708ef7593aad6a8
zulhafizazmi <81027436+zulhafizazmi@users.noreply.github.com>
2021-03-31 21:28:30 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,48 +3,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ClassBag a = new ClassBag();
-		System.out.println(a.Brand);
-		
-		ClassBag b = new ClassBag();
-		System.out.println(b.Colour);
-		
-		ClassBag c = new ClassBag();
-		System.out.println(c.Weight + "kg");
-		
-		ClassBag d = new ClassBag();
-		d.Waterresistance();
+		Bag adidas = new Bag();
+		adidas.brand = "adidas";
+		adidas.colour = "white";
+		System.out.println("brand : " + adidas.brand);
+		System.out.println("colour : " + adidas.colour);
+		adidas.waterresistance();
 		
 		System.out.println();
 		
-		ClassBag1 e = new ClassBag1();
-		System.out.println(e.Brand);
-		
-		ClassBag1 f = new ClassBag1();
-		System.out.println(f.Colour + " in colour");
-		
-		ClassBag1 g = new ClassBag1();
-		System.out.println(g.weight + "kg");
-		
-		ClassBag1 h = new ClassBag1();
-		h.Waterresistance();
+		Bag nike = new Bag();
+		nike.brand = "nike";
+		nike.colour = "black";
+		System.out.println("brand : " + nike.brand);
+		System.out.println("colour : " + nike.colour);
+		nike.waterresistance();
 		
 		System.out.println();
-		
-		ClassBag2 i = new ClassBag2();
-		System.out.println(i.Brand);
-		
-		ClassBag2 j = new ClassBag2();
-		System.out.println(j.Colour + " in colour");
-		
-		ClassBag2 k = new ClassBag2();
-		System.out.println(k.weight + "kg");
-		
-		ClassBag2 l = new ClassBag2();
-		l.Waterresistance();
-		
-		
-		
 		
 
 	}
